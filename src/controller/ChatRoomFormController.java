02d452b50f4147d8e3d101de2a4fd3ce3a218ad8
idlakshan <id.lakshan21@gmail.com>
2022-08-16 03:28:52 +0530

    Create ChatRoom Form

---
+++ src/controller/ChatRoomFormController.java
@@ -0,0 +1,22 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextArea;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class ChatRoomFormController {
+
+    public TextField txtMessage;
+    public JFXTextArea txtAreaMessage;
+    public Label lblName;
+    public AnchorPane chatRoomContext;
+
+
+
+    public void btnSendMsgOnAction(ActionEvent event) {
+
+    }
+}
--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -1,5 +1,32 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginFormController {
 
+    public AnchorPane loginPageContext;
+    public TextField txtUsername;
+
+
+    static String username;
+    public void btnLoginOnAction(ActionEvent event) throws IOException {
+        username=txtUsername.getText();
+
+        Stage stage1 = (Stage) txtUsername.getScene().getWindow();
+        stage1.close();
+        Stage stage2=new Stage();
+        stage2.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/chatRoomForm.fxml"))));
+        stage2.setResizable(false);
+        stage2.setTitle("ChitChat");
+        stage2.centerOnScreen();
+        stage2.show();
+
+    }
 }
