148841562444f2b21be7f08be70b76c7bc22c228
idlakshan <id.lakshan21@gmail.com>
2022-08-16 01:03:30 +0530

    Add Login interface

---
+++ src/ClientOne.java
@@ -0,0 +1,30 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+import java.io.IOException;
+
+
+public class ClientOne extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        System.out.println("working");
+
+        Parent parent= FXMLLoader.load(this.getClass().getResource("view/loginForm.fxml"));
+        Scene scene=new Scene(parent);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Login");
+        primaryStage.centerOnScreen();
+        primaryStage.show();
+
+
+    }
+}
